140cc3d4cecfe08b880ca4b77d0563cd725f6300
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2025-02-07 16:21:51 +0200

    responsible designer is added

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+
+const App = () => {
+  return (
+    <div className="bg-gray-800">
+    <div className="bg-gray-800 w-11/12 py-10 mx-auto grid grid-cols-3 gap-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+      <div className="bg-gray-300 py-3  px-5 rounded-md ">
+      <h1 className="uppercase font-roboto font-bold text-xl py-2 text-blue-400">biology course</h1>
+      <p className="text-white font-semibold py-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quibusdam suscipit possimus, quod quidem voluptates ducimus, dicta exercitationem soluta porro, neque ipsam praesentium doloribus! Debitis nulla dolores quibusdam iure hic!</p>
+        <div className=" ">
+        <button className="bg-blue-500 rounded-md  shadow-md hover:bg-gray-600 hover:text-blue-400 text-white px-3 py-2 font-bold ">view more</button>
+        </div>
+      </div>
+      <div className="bg-gray-300 py-3  px-5 rounded-md ">
+      <h1 className="uppercase font-roboto font-bold text-xl py-2 text-blue-400">computer science</h1>
+      <p className="text-white font-semibold py-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quibusdam suscipit possimus, quod quidem voluptates ducimus, dicta exercitationem soluta porro, neque ipsam praesentium doloribus! Debitis nulla dolores quibusdam iure hic!</p>
+        <div className=" ">
+        <button className="bg-blue-500 rounded-md shadow-md hover:bg-gray-600 hover:text-blue-400 text-white px-3 py-2 font-bold ">view more</button>
+        </div>
+      </div>
+      <div className="bg-gray-300 py-3  px-5 rounded-md ">
+      <h1 className="uppercase font-roboto font-bold text-xl py-2 text-blue-400">software developer</h1>
+      <p className="text-white font-semibold py-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quibusdam suscipit possimus, quod quidem voluptates ducimus, dicta exercitationem soluta porro, neque ipsam praesentium doloribus! Debitis nulla dolores quibusdam iure hic!</p>
+        <div className=" ">
+        <button className="bg-blue-500 rounded-md shadow-md hover:bg-gray-600 hover:text-blue-400 text-white px-3 py-2 font-bold ">view more</button>
+        </div>
+      </div>
+      <div className="bg-gray-300 py-3  px-5 rounded-md ">
+      <h1 className="uppercase font-roboto font-bold text-xl py-2 text-blue-400">geography course</h1>
+      <p className="text-white font-semibold py-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quibusdam suscipit possimus, quod quidem voluptates ducimus, dicta exercitationem soluta porro, neque ipsam praesentium doloribus! Debitis nulla dolores quibusdam iure hic!</p>
+        <div className=" ">
+        <button className="bg-blue-500 rounded-md shadow-md hover:bg-gray-600 hover:text-blue-400 text-white px-3 py-2 font-bold ">view more</button>
+        </div>
+      </div>
+      <div className="bg-gray-300 py-3  px-5 rounded-md ">
+      <h1 className="uppercase font-roboto font-bold text-xl py-2 text-blue-400">history course</h1>
+      <p className="text-white font-semibold py-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quibusdam suscipit possimus, quod quidem voluptates ducimus, dicta exercitationem soluta porro, neque ipsam praesentium doloribus! Debitis nulla dolores quibusdam iure hic!</p>
+        <div className=" ">
+        <button className="bg-blue-500 rounded-md shadow-md hover:bg-gray-600 hover:text-blue-400 text-white px-3 py-2 font-bold ">view more</button>
+        </div>
+      </div>
+      <div className="bg-gray-300 py-3  px-5 rounded-md ">
+      <h1 className="uppercase font-roboto font-bold text-xl py-2 text-blue-400">course of to day</h1>
+      <p className="text-white font-semibold py-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quibusdam suscipit possimus, quod quidem voluptates ducimus, dicta exercitationem soluta porro, neque ipsam praesentium doloribus! Debitis nulla dolores quibusdam iure hic!</p>
+        <div className=" ">
+        <button className="bg-blue-500 rounded-md shadow-md hover:bg-gray-600 hover:text-blue-400 text-white px-3 py-2 font-bold ">view more</button>
+        </div>
+      </div>
+      
+    </div>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),   tailwindcss()],
+  server:{
+    port:3000,
+  },
+  screens: {
+    'xs': '480px',  // Adding an extra-small breakpoint
+    '3xl': '1600px', // Adding an extra-large breakpoint
+  },
+})
